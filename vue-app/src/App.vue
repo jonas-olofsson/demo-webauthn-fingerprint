46e378da6e812a6e69a055abcd9ce2b01d7d200a
Jonas Olofsson <jonas@jonasolofsson.com>
2019-08-23 15:34:57 +0100

    update first page and adde register button

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Logo" src="./assets/logo.svg">
+    <Register/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Register from './components/Register.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Register
   }
 }
 </script>
---
+++ vue-app/src/components/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="register">
+     <button>Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
